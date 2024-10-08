31c8315a8d0d170febbee5b1654abc03b4b0e75b
saki0411 <chipdale411@gmail.com>
2024-10-08 13:29:52 +0900

    Initial Commit

---
+++ HaishinList/HaishinListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaishinListApp.swift
+//  HaishinList
+//
+//  Created by saki on 2024/10/08.
+//
+
+import SwiftUI
+
+@main
+struct HaishinListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
